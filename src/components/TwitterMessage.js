96f0e2dee18ba352381717508e2738f8799f5c65
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-06-30 11:17:14 +0930

    implement twitter message class

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,35 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
 
+  handleChangeValue = (maxChars, event) => {
+    if (event.target.value.length <= maxChars) {
+      this.setState({
+        message: event.target.value,
+        remainingChar: maxChars - event.target.value.length,
+      });
+    }
+  };
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.message}
+          onChange={(event) =>
+            this.handleChangeValue(this.props.maxChars, event)
+          }
+        />
+        <span>
+          Remaining characters {this.props.maxChars - this.state.message.length}
+        </span>
       </div>
     );
   }
